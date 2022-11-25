53298fac93bf46e51f98739480dba90a693ef9ab
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-25 08:58:20 -0300

    add font

--- src/styles/global.js
+++ src/styles/global.js
@@ -10,6 +10,13 @@ export default createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
     color: ${({ theme }) => theme.COLORS.WHITE};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto Slab', sans-serif;
+    font-size: 16px;
+    outline: none;
   }
 
   a {
