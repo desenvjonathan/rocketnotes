016248ba2cb1b4561be2e80516dae37c021afcf9
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-29 01:03:02 -0300

    add img profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,18 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
   return (
     <Container>
-      
+      <Profile>
+        <img 
+        src="https://github.com/desenvjonathan.png" 
+        alt="Foto de usuário" />
+
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Jonathan Ramos</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -14,6 +14,31 @@ export const Container = styled.header`
   justify-content: space-between;
 
   padding: 0 80px;
+`
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
 
-  background: red;
+  > img {/*o sinal de maior que é para garantir que será estilizada 
+  a imagem de profile e não uma outra imagem */
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span { /*aqui dentro conseguimos fazer uma castata e acessar os outros elementos*/
+      font-size: 14px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    strong { 
+      font-size: 18px;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
 `
