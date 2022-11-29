b961635e8bbac3c09176cfe002bf41803998fb32
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-29 15:08:50 -0300

    add component section / studied children in react

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
-import { Container } from './styles.js'
+import { Container, Links } from './styles.js'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
 
 export function Details/*TODO COMPONENTE PRECISA COMEÇAR COM LETRA MAIÚSCULA */(){
   
@@ -8,7 +9,19 @@ export function Details/*TODO COMPONENTE PRECISA COMEÇAR COM LETRA MAIÚSCULA *
     /*Dentro de um componente, só podemos colocar UM elemento, nesse caso a Div
     e só com a Div que podemos colocar mais elementos dentro*/
     <Container>
-      <Header></Header>
+      <Header/>
+      
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="https://github.com/desenvjonathan">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/jonathan-ramos-p/">LinkedIn</a>
+          </li>
+        </Links>
+      </Section>
+
       <Button title="Voltar"/>
     </Container>
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -10,3 +10,15 @@ export const Container = styled.div`
   "header"
   "content"
 `;
+
+export const Links = styled.ul`
+  list-style: none;
+
+  > li {
+    margin-top: 12px;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
+`;
