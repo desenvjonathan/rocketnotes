22062df4bd3e4f0bd5e6dfd640d9cd708b1963ad
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-30 01:14:23 -0300

    styling page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
@@ -26,7 +27,8 @@ export function Home() {
       </Content>
 
       <NewNote>
-
+        <FiPlus/>
+        Criar Nota
       </NewNote>
     </Container>
   )
