a34e1bd0e8c17f135f418d565f0e95dbc5bcea3c
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-30 00:55:02 -0300

    add logo

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,21 +16,31 @@ export const Container = styled.div`
 `
 export const Brand = styled.div`
   grid-area: brand;
-  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
 `
 export const Menu = styled.ul`
   grid-area: menu;
-  background-color: green;
 `
 export const Search = styled.div`
   grid-area: search;
-  background-color: violet;
 `
 export const Content = styled.div`
   grid-area: content;
-  background-color: blue;
 `
 export const NewNote= styled.button`
   grid-area: newnote;
-  background-color: yellow;
 `
