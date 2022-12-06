651b27ef6f8bbffa73232c3e52c49aace9e54223
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-12-06 13:34:36 -0300

    add image avatar profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,9 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 
 export function Profile() {
@@ -16,6 +16,22 @@ export function Profile() {
       </header>
 
       <Form>
+        <Avatar>
+          <img 
+          src="https://github.com/desenvjonathan.png" 
+          alt="Foto do usuário" 
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera/>
+
+            <input
+              id="avatar"
+              type="file"
+            />
+          </label>
+        </Avatar>
+
         <Input
           placeholder="Nome"
           type="text"
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -24,4 +24,49 @@ export const Form = styled.form`
   max-width: 340px;
   margin: 30px auto 0;
 
+  > div:nth-child(4){
+    margin-top: 24px;
+  }
+`
+
+export const Avatar = styled.div`
+  position: relative;
+  margin: -124px auto 32px;
+
+  width: 186px;
+  height: 186px;
+
+  > img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({ theme }) => theme.COLORS.ORANGE};
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+
+    cursor: pointer;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    }
+  }
 `
