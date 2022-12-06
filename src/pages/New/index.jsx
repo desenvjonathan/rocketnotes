b48299b7d3ad5bf03fc56f7067f49e9e13a7b6b4
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-12-06 13:51:56 -0300

    header new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,23 @@
 import { Header } from "../../components/Header"
+import { Input } from "../../components/Input"
 
-import { Container } from "./styles"
+import { Container, Form } from "./styles"
 
 export function New() {
   return (
     <Container>
       <Header/>
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar Nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+
+          <Input placeholder="Título"/>
+        </Form>
+      </main>
     </Container>
   )
 }
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -10,3 +10,20 @@ export const Container = styled.div`
   "header"
   "Content";
 `
+
+export const Form = styled.form`
+  max-width: 550px;
+  margin: 38px auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 36px;
+    a {
+      font-size: 20px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+  }
+`
