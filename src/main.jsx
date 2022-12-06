8ba561784d6ee15a5a02a7b40ba664f73531b4e1
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-12-06 13:48:23 -0300

    add page New

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
-import { Profile } from './pages/Profile' 
+import { New } from './pages/New' 
 /*usar chaves para criar um import NOMEADO e deixar o código padronizado*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header"
+
+import { Container } from "./styles"
+
+export function New() {
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  )
+}
