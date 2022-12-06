5caa9a2bd9bf79ef8fd48ecd15cf8f9219370a93
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-12-06 13:59:39 -0300

    add component TextArea

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header"
 import { Input } from "../../components/Input"
+import { TextArea } from "../../components/TextArea"
 
 import { Container, Form } from "./styles"
 
@@ -16,6 +17,7 @@ export function New() {
           </header>
 
           <Input placeholder="Título"/>
+          <TextArea placeholder="Observações"/>
         </Form>
       </main>
     </Container>
