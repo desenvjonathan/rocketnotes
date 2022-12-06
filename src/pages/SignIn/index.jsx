7b713c829b2532e6da170d4909f3fee618eaba98
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-12-06 12:36:06 -0300

    add form SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,37 @@
-import { Container } from "./styles";
+import { FiMail, FiLock} from "react-icons/fi"
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+
+import { Container, Form } from "./styles"
 
 export function SignIn() {
   return (
     <Container>
-      
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Entrar"/>
+
+        <a href="#">
+          Criar Conta
+        </a>
+      </Form>
     </Container>
   )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -6,3 +6,36 @@ height: 100vh;
 display: flex;
 align-items: stretch;
 `
+
+export const Form = styled.form`
+  padding: 0 136px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+
+  > h1 {
+    font-size: 48px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  > h2 {
+    font-size: 24px;
+    margin-top: 84px;
+    margin-bottom: 24px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  > a {
+    margin-top: 124px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+    
+  }
+`
